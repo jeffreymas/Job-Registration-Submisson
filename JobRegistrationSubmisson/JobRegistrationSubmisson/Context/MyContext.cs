9bcb2eb8d29d7a78f467c551e7aeb13071c337b0
Jepri <55873259+jeffreymas@users.noreply.github.com>
2020-09-14 15:27:24 +0700

    User And Role

---
+++ JobRegistrationSubmisson/JobRegistrationSubmisson/Context/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using JobRegistrationSubmisson.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobRegistrationSubmisson.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+        public DbSet<Accounts> Accounts { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<AccRoles> AccRoles { get; set; }
+        
+    }
+}
---
+++ JobRegistrationSubmisson/JobRegistrationSubmisson/Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobRegistrationSubmisson.Models
+{
+    [Table ("Tb_Roles")]
+    public class Roles : IdentityRole
+    {
+    }
+}
